e9c9774e635c7c8fade3d416af8138c168b4ebfc
noimin0610 <mii.informationsystems@gmail.com>
2019-09-06 18:13:02 +0900

    雑なタイトル検索

--- api/src/app.py
+++ api/src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 import sqlalchemy
 import os
 from .utils import get_plural
@@ -19,7 +19,6 @@ class Game(Resource):
         for row in result:
             tags.append(row[f'{column}_name'])
         return ','.join(tags)
-
 
     def get(self, game_id):
         q = f"SELECT * FROM games WHERE id = {game_id}"
@@ -41,15 +40,47 @@ class Game(Resource):
         return {
             'status': 200,
             'message': 'OK',
-            'games': {column: game[column] for column in game_columns}
+            'game': {column: game[column] for column in game_columns}
         }
     
+
+class Games(Resource):
+    
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('title', type=str)
+        args = parser.parse_args()
+        title = args['title']
+        q = f"SELECT id FROM games WHERE title LIKE '%{title}%'"
+        result = engine.execute(sqlalchemy.text(q))
+        game_ids = []
+        for row in result:
+            game_ids.append(row['id'])
+        print(game_ids)
+
+        if not game_ids:
+            return {
+                'status': 404, 
+                'message': f'game including {title} in title is not found'
+            }, 404
+
+        games = []
+        for game_id in game_ids:
+            games.append(Game().get(game_id)['game'])
+
+        return {
+            'status': 200,
+            'message': 'OK',
+            'games': games
+        }
+
 
 def create_app():
     app = Flask(__name__)
 
     api = Api(app)
     api.add_resource(Game, '/games/<int:game_id>')
+    api.add_resource(Games, '/games')
 
     return app
 
