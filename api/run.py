158f6cb0e6b9751db891dfba24b38a4fe8eec826
fuurin <komatsuemgbr0624@gmail.com>
2019-09-07 18:10:03 +0900

    key name changed

--- api/run.py
+++ api/run.py
@@ -4,7 +4,7 @@ import os
 if __name__ == '__main__':
     if os.environ["SSL_DIR"]:
         ssl_dir = os.environ["SSL_DIR"]
-        ssl_context = (f"{ssl_dir}/server.crt", f'{ssl_dir}/server.key')
+        ssl_context = (f"{ssl_dir}/fullchain.pem", f'{ssl_dir}/privkey.pem')
         app.run(host='0.0.0.0', port=334, ssl_context=ssl_context, threaded=True, debug=False)
     else:
         app.run(debug=True)
