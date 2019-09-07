a30e9f46fcae0ff59f038e91c3724af7f50c43a4
fuurin <komatsuemgbr0624@gmail.com>
2019-09-07 18:02:59 +0900

    run.py for ssl

--- api/run.py
+++ api/run.py
@@ -1,5 +1,10 @@
 from src.app import app
-
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if os.environ["SSL_DIR"]:
+        ssl_dir = os.environ["SSL_DIR"]
+        ssl_context = (f"{ssl_dir}/server.crt", f'{ssl_dir}/server.key')
+        app.run(host='0.0.0.0', port=334, ssl_context=ssl_context, threaded=True, debug=False)
+    else:
+        app.run(debug=True)
