d97081b95293096b628edc08a421d62d4724f8a3
noimin0610 <mii.informationsystems@gmail.com>
2019-09-19 12:44:29 +0900

    存在判定の方法を修正．あとちょっとリファクタリング

--- crawler/galook/galook/pipelines.py
+++ crawler/galook/galook/pipelines.py
@@ -28,22 +28,33 @@ class PostgresPipeline(object):
     def close_spider(self, spider: scrapy.Spider):
         pass
 
+    def execute_sql(self, sql, args):
+        try:
+            curs = self.conn.cursor()
+            curs.execute(sql, args)
+        except psycopg2.errors.InFailedSqlTransaction as e:
+            print(e)
+            print(sql)
+        return curs
+
+    def exists(self, curs):
+        return curs.description is not None
+
+    def is_new_standard(self, item, standard_price, standard_id):
+        return (item['price'] != -1 and item['price'] < standard_price) or (item['price'] == standard_price and item['id'] < standard_id)
+
     def process_item(self, item, spider):
         # 存在判定
-        sql = "SELECT id FROM editions WHERE id = %s;"
-        curs = self.conn.cursor()
-        curs.execute(sql, (item['id'], ))
-        if curs.fetchone() is not None:
+        curs = self.execute_sql("SELECT id FROM editions WHERE id = %s;", (item['id'], ))
+        if self.exists(curs):
             return item
 
         # editions テーブル
-        sql = textwrap.dedent("""\
-        INSERT INTO editions (id, title, brand, price, release_date, story, url) \
-        VALUES (%s, %s, %s, %s, %s, %s, %s);\
-        """)
-        curs = self.conn.cursor()
-        curs.execute(
-            sql, (
+        curs = self.execute_sql(
+            textwrap.dedent("""\
+            INSERT INTO editions (id, title, brand, price, release_date, story, url) \
+            VALUES (%s, %s, %s, %s, %s, %s, %s);\
+            """), (
                 item['id'], item['title'], item['brand'], item['price'], 
                 item['release_date'], item['story'], item['url']
             )
@@ -51,87 +62,90 @@ class PostgresPipeline(object):
 
         # # categories テーブル
         for category in item['category']:
-            sql = textwrap.dedent("""\
-            INSERT INTO categories (edition_id, category_name) \
-            VALUES (%s, %s);\
-            """)
-            curs = self.conn.cursor()
-            curs.execute(sql, (item['id'], category))
+            curs = self.execute_sql(
+                textwrap.dedent("""\
+                INSERT INTO categories (edition_id, category_name) \
+                VALUES (%s, %s);\
+                """), 
+                (item['id'], category)
+            )
 
         # # subgenres テーブル
         for subgenre in item['subgenre']:
-            sql = textwrap.dedent("""\
-            INSERT INTO subgenres (edition_id, subgenre_name) \
-            VALUES (%s, %s);\
-            """)
-            curs = self.conn.cursor()
-            curs.execute(sql, (item['id'], subgenre))
+            curs = self.execute_sql(
+                textwrap.dedent("""\
+                INSERT INTO subgenres (edition_id, subgenre_name) \
+                VALUES (%s, %s);\
+                """), 
+                (item['id'], subgenre)
+            )
 
         # # writers テーブル
         for writer in item['writer']:
-            sql = textwrap.dedent("""\
-            INSERT INTO writers (edition_id, writer_name) \
-            VALUES (%s, %s);\
-            """)
-            curs = self.conn.cursor()
-            curs.execute(sql, (item['id'], writer))
+            curs = self.execute_sql(
+                textwrap.dedent("""\
+                INSERT INTO writers (edition_id, writer_name) \
+                VALUES (%s, %s);\
+                """), 
+                (item['id'], writer)
+            )
 
         # games テーブル・editions テーブルの game_id 列
         if item['story']:
-            sql = textwrap.dedent("""\
-            SELECT id, standard_edition_id FROM games WHERE story = %s;
-            """)
-            curs = self.conn.cursor()
-            curs.execute(sql, (item['story'], ))
-            row = curs.fetchone()
-            if row is None:
-                standard_id = None
+            curs = self.execute_sql(
+                textwrap.dedent("""\
+                SELECT id, standard_edition_id FROM games WHERE story = %s;
+                """), 
+                (item['story'], )
+            )
+            if self.exists(curs):
+                game_id, standard_id = curs.fetchone()[0]
             else:
-                game_id, standard_id = row
+                standard_id = None
         else:
             standard_id = None
         if not standard_id:
-            sql = textwrap.dedent("""\
-            INSERT INTO games (title, brand, story, standard_edition_id) \
-            VALUES (%s, %s, %s, %s);\
-            """)
-            curs = self.conn.cursor()
-            curs.execute(
-                sql, (
+            curs = self.execute_sql(
+                textwrap.dedent("""\
+                INSERT INTO games (title, brand, story, standard_edition_id) \
+                VALUES (%s, %s, %s, %s);\
+                """), (
                     item['title'], item['brand'], item['story'], item['id']
                 )
             )
 
-            sql = textwrap.dedent("""\
-            UPDATE editions \
-            SET game_id = (SELECT MAX(id) FROM games)\
-            WHERE id = %s;
-            """)
-            curs = self.conn.cursor()
-            curs.execute(sql, (item['id'], ))
+            curs = self.execute_sql(
+                textwrap.dedent("""\
+                UPDATE editions \
+                SET game_id = (SELECT MAX(id) FROM games)\
+                WHERE id = %s;
+                """), 
+                (item['id'], )
+            )
         else:
-            sql = textwrap.dedent("""\
-            UPDATE editions \
-            SET game_id = %s\
-            WHERE id = %s;
-            """)
-            curs = self.conn.cursor()
-            curs.execute(sql, (game_id, item['id']))
+            curs = self.execute_sql(
+                textwrap.dedent("""\
+                UPDATE editions \
+                SET game_id = %s\
+                WHERE id = %s;
+                """), 
+                (game_id, item['id'])
+            )
 
-            sql = textwrap.dedent("""\
-            SELECT price FROM editions WHERE id = %s;
-            """)
-            curs = self.conn.cursor()
-            curs.execute(sql, (standard_id, ))
+            curs = self.execute_sql(
+                "SELECT price FROM editions WHERE id = %s;", 
+                (standard_id, )
+            )
             standard_price = curs.fetchone()[0]
-            if (item['price'] != -1 and item['price'] < standard_price) or (item['price'] == standard_price and item['id'] < standard_id):
-                sql = textwrap.dedent("""\
-                UPDATE games \
-                SET standard_edition_id = %s\
-                WHERE standard_edition_id = %s;
-                """)
-                curs = self.conn.cursor()
-                curs.execute(sql, (item['id'], standard_id))
+            if self.is_new_standard(item, standard_price, standard_id):
+                curs = self.execute_sql(
+                    textwrap.dedent("""\
+                    UPDATE games \
+                    SET standard_edition_id = %s\
+                    WHERE standard_edition_id = %s;
+                    """), 
+                    (item['id'], standard_id)
+                )
         
         self.conn.commit()
 
